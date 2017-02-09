d1f757ccf94f7e68d7b78a8e56ec7e01f0bc2887
rocioparra <rparra@itba.edu.ar>
2017-02-09 15:01:13 -0300

    ANDA
    nombres de las funciones corregidos
    enum de luces en el c para que no se mezcle con el otro

--- al_display_management.h
+++ al_display_management.h
@@ -5,22 +5,21 @@
 #include "allegro5/allegro_image.h"
 #include "allegro5/allegro_audio.h"
 #include "allegro5/allegro_acodec.h"
-#include "allegro5/allegro_font.h" 
-#include "allegro5/allegro_ttf.h" 
-#include "allegro5/allegro_native_dialog.h"
+#include "allegro5/allegro_font.h" //manejo de fonts
+#include "allegro5/allegro_ttf.h" //Manejo de ttfs
+#include "allegro5/allegro_native_dialog.h" //Manejo de ttfs
 #include "al_img_prop.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <strings.h>
 
 #define EXIT_SIMON  -1
 
-enum src_of_event { NO_SOURCE, SOURCE_MOUSE, SOURCE_KB }; 
+enum src_of_event { NO_SOURCE, SOURCE_MOUSE, SOURCE_KB }; //source of event, de donde tomar input
 
-int inicializacion (int);
+int al_inicializacion (int);
 
-void finalizacion (void);
+void al_finalizacion (void);
 
 /* Funcion get_event
  * 
@@ -34,7 +33,7 @@ void finalizacion (void);
  *  3: flecha abajo
  * -1: tecla escape 
  */
-int get_event (void);
+int al_get_event (void);
 
 /* Funcion turn_light_on
  * 
@@ -45,7 +44,7 @@ int get_event (void);
  *  3: enciende solo la luz inferior
  *  4: apaga todas las luces
  */
-void turn_light_on (int);
+void al_turn_light_on (int);
 
 /* Funcion play_beep
  * 
@@ -56,58 +55,53 @@ void turn_light_on (int);
  *  3: prende el sonido del boton inferior
  *  4: silencio
  */
-void play_beep (int);
+void al_play_beep (int);
 
-
-/* Funcion kb_or_mouse 
+/*
  * 
- * En su primera invocacion, muestra un mensaje en pantalla indicando al usuario
- * que si desea jugar con el teclado, aprete cualquier tecla, y en caso contrario, 
- * para jugar con el mouse, que aprete cualquier boton 
- * 
- * DEVUELVE: el codigo correspondiente a la eleccion del usuario (int):
- *  1: mouse
- *  2: teclado
- * 
- * En las invocaciones siguientes se saltea el mensaje de eleccion. Se devuelve 
- * el codigo correspondiente al metodo de input seleccionado en la primera 
- * invocacion
  */
-int kb_or_mouse (void);
+int al_b_or_mouse (void);
 
 /* Funcion new_highscore
  * 
  * RECIBE: int con el nuevo highscore
- * 
- * Abre un pop up indicando que se ha llegado a un nuevo highscore y cual es el 
- * valor del mismo.
- * Reproduce musica mientras que el pop up este abierto
+ * Abre un pop up indicando que se ha llegado
+ * a un nuevo highscore y cual es el valor 
+ * del mismo
  */
-void new_highscore(int);
+void al_new_highscore(int);
+
+/* Funcion show_highscore
+ * 
+ * Carga en el display principal
+ * cual es el highscore actual
+ * (no lo muestra, solamente lo 
+ * carga. Para mostrarlo, es 
+ * necesario invocar a la funcion
+ * al_flip_display(). )
+ */
+void draw_score_and_highscore ();
 
 /* Funcion wrong_sequence
  * 
- * Lleva a cabo la animacion y reproduce el sonido correspondientes al momento 
- * en el que el usuario ingresa una secuencia incorrecta
+ * Lleva a cabo la animacion y 
+ * reproduce el sonido correspondientes
+ * a una secuencia incorrecta 
+ * sincronizadamente
  */
-void wrong_sequence (void);
+void al_wrong_attempt (void);
 
 /* Funcion correct_sequence
  * 
- * Lleva a cabo la animacion y reproduce el sonido correspondientes al momento 
- * en el que el usuario ingresa una secuencia correcta
+ * Lleva a cabo la animacion y 
+ * reproduce el sonido correspondientes
+ * a una secuencia correcta sincronizadamente
  */
-void correct_sequence (void);
+void al_correct_attempt (void);
 
-/* Funcion draw_bg_and_hs
- * 
- * Carga en el display principal el fondo y cuales son el puntaje y el highscore 
- * actual (no lo muestra, solamente lo carga. Para mostrarlo, es necesario invocar 
- * a la funcion al_flip_display(). )
- */
-void draw_background_and_scores (void);  
+void draw_bg_and_hs (void);  
 
-void update_score(int);
+void al_update_score(int);
 
 
 #endif /* AL_DISPLAY_MANAGEMENT_H */
